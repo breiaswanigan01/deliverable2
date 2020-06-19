dc6eb7fbfcb0c657f67034b82af28671a2e7c259
breia <breiaswanigan1@gmail.com>
2020-06-19 08:37:03 -0400

    finishedd

--- js/script.js
+++ js/script.js
@@ -22,64 +22,42 @@
 
 // console.log (result) -- will print out string
 
-var vacationType, destination;
 
-var vacationType  = ["Musical", "Tropical", "Adventerous"];
-var vacationType = ["New Orleans", "Beach Vacation in Mexico", "Whitewater Rafting the Grand Canyon"];
-var destination;
 
-var vacationType= "Musical"
-var vacationType= "Tropical"
-var vacationType= "Adventerous"
-
-var vacationType= prompt("What kind of trip would you like to go on?");
+var vacationType= prompt("What kind of trip would you like to go on, Musical, Tropical, or Adventerous?");
 if (vacationType == "Musical") {
    destination= "New Orleans";
 } 
  if (vacationType == "Tropical") {
    destination = "Beach Vacation in Mexico";
 } 
- if (vacationType == "Adventerous") {
+ if  (vacationType == "Adventerous") {
    destination= "Whitewater Rafting the Grand Canyon";
 } 
 
 
-var groupSize, travel;
-
-var groupSize = [1,2,3,4,5,6,7,8,9,10];
-var  groupSize = ["First Class", "Helicopter", "Charter Flight"];
 
 
 
 var groupSize= prompt("How many people are in your group?");
-if (groupSize == 1, 2) {
- travel= "First Class";
+if (groupSize == 1,2) { 
+ travelSuggestion= "First Class"
 }
-if (groupSize == 3, 4, 5) {
-    travel= "Helicopter";
+ if (groupSize > 2 < 6) {
+    travelSuggestion= "Helicopter"
 }
-if (groupSize == 6, 7, 8, 9, 10) {
- travel= "Charter Flight"; 
+if  (groupSize > 6) {
+ travelSuggestion= "Charter Flight" 
 }
 
-var travel= prompt("What travel suggestion would you like?");
-if (travel == "First Class") {
-    groupSize= 1,2;
-   }
-   if (travel == "Helicopter") {
-    groupSize= 3,4,5;
-   }if (travel == "Charter Flight") {
-    groupSize= 6,7,8,9,10;
-   }
 
 
 
 
-var result;
-var result= ("vacationType", "+", "groupSize");
-console.log('Since you are a group of '+ groupSize + ' '+ 'going on a ' +
- vacationType + ' '+ 'trip ' + 'you should take a '  + travel + ' '+ 'to' + ' ' + destination);
 
+var result= "Since you are a group of "+ groupSize  +  " going on a " +
+ vacationType  +  " trip " + "you should take a "  + travelSuggestion + " to " + destination;
+console.log(result);
 
 
 
